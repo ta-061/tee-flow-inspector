884a5d336848664c439e26a4cd2bffdf1eac6452
ta-061 <cstt22111@g.nihon-u.ac.jp>
2025-06-15 17:25:52 +0900

    Add entrypoint script for latte-dev container

---
+++ docker/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# entrypoint for latte-dev container
+set -e
+
+# ----- (任意) Python 仮想環境を自動有効化したい場合 -----
+# if [ -f /workspace/.venv/bin/activate ]; then
+#   source /workspace/.venv/bin/activate
+# fi
+
+exec "$@"
