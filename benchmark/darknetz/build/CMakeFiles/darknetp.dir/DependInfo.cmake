31875877eca5d464506999976c6f1f7d347cf47a
ta-061 <cstt22111@g.nihon-u.ac.jp>
2025-07-17 22:25:05 +0000

    プロンプト改良実行結果

---
+++ benchmark/darknetz/build/CMakeFiles/darknetp.dir/DependInfo.cmake
@@ -0,0 +1,83 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspace/benchmark/darknetz/host/examples/art.c" "CMakeFiles/darknetp.dir/host/examples/art.o" "gcc" "CMakeFiles/darknetp.dir/host/examples/art.o.d"
+  "/workspace/benchmark/darknetz/host/examples/captcha.c" "CMakeFiles/darknetp.dir/host/examples/captcha.o" "gcc" "CMakeFiles/darknetp.dir/host/examples/captcha.o.d"
+  "/workspace/benchmark/darknetz/host/examples/cifar.c" "CMakeFiles/darknetp.dir/host/examples/cifar.o" "gcc" "CMakeFiles/darknetp.dir/host/examples/cifar.o.d"
+  "/workspace/benchmark/darknetz/host/examples/classifier.c" "CMakeFiles/darknetp.dir/host/examples/classifier.o" "gcc" "CMakeFiles/darknetp.dir/host/examples/classifier.o.d"
+  "/workspace/benchmark/darknetz/host/examples/coco.c" "CMakeFiles/darknetp.dir/host/examples/coco.o" "gcc" "CMakeFiles/darknetp.dir/host/examples/coco.o.d"
+  "/workspace/benchmark/darknetz/host/examples/darknet.c" "CMakeFiles/darknetp.dir/host/examples/darknet.o" "gcc" "CMakeFiles/darknetp.dir/host/examples/darknet.o.d"
+  "/workspace/benchmark/darknetz/host/examples/detector.c" "CMakeFiles/darknetp.dir/host/examples/detector.o" "gcc" "CMakeFiles/darknetp.dir/host/examples/detector.o.d"
+  "/workspace/benchmark/darknetz/host/examples/go.c" "CMakeFiles/darknetp.dir/host/examples/go.o" "gcc" "CMakeFiles/darknetp.dir/host/examples/go.o.d"
+  "/workspace/benchmark/darknetz/host/examples/instance-segmenter.c" "CMakeFiles/darknetp.dir/host/examples/instance-segmenter.o" "gcc" "CMakeFiles/darknetp.dir/host/examples/instance-segmenter.o.d"
+  "/workspace/benchmark/darknetz/host/examples/lsd.c" "CMakeFiles/darknetp.dir/host/examples/lsd.o" "gcc" "CMakeFiles/darknetp.dir/host/examples/lsd.o.d"
+  "/workspace/benchmark/darknetz/host/examples/nightmare.c" "CMakeFiles/darknetp.dir/host/examples/nightmare.o" "gcc" "CMakeFiles/darknetp.dir/host/examples/nightmare.o.d"
+  "/workspace/benchmark/darknetz/host/examples/regressor.c" "CMakeFiles/darknetp.dir/host/examples/regressor.o" "gcc" "CMakeFiles/darknetp.dir/host/examples/regressor.o.d"
+  "/workspace/benchmark/darknetz/host/examples/rnn.c" "CMakeFiles/darknetp.dir/host/examples/rnn.o" "gcc" "CMakeFiles/darknetp.dir/host/examples/rnn.o.d"
+  "/workspace/benchmark/darknetz/host/examples/segmenter.c" "CMakeFiles/darknetp.dir/host/examples/segmenter.o" "gcc" "CMakeFiles/darknetp.dir/host/examples/segmenter.o.d"
+  "/workspace/benchmark/darknetz/host/examples/super.c" "CMakeFiles/darknetp.dir/host/examples/super.o" "gcc" "CMakeFiles/darknetp.dir/host/examples/super.o.d"
+  "/workspace/benchmark/darknetz/host/examples/tag.c" "CMakeFiles/darknetp.dir/host/examples/tag.o" "gcc" "CMakeFiles/darknetp.dir/host/examples/tag.o.d"
+  "/workspace/benchmark/darknetz/host/examples/yolo.c" "CMakeFiles/darknetp.dir/host/examples/yolo.o" "gcc" "CMakeFiles/darknetp.dir/host/examples/yolo.o.d"
+  "/workspace/benchmark/darknetz/host/main.c" "CMakeFiles/darknetp.dir/host/main.o" "gcc" "CMakeFiles/darknetp.dir/host/main.o.d"
+  "/workspace/benchmark/darknetz/host/src/activation_layer.c" "CMakeFiles/darknetp.dir/host/src/activation_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/activation_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/activations.c" "CMakeFiles/darknetp.dir/host/src/activations.o" "gcc" "CMakeFiles/darknetp.dir/host/src/activations.o.d"
+  "/workspace/benchmark/darknetz/host/src/avgpool_layer.c" "CMakeFiles/darknetp.dir/host/src/avgpool_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/avgpool_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/batchnorm_layer.c" "CMakeFiles/darknetp.dir/host/src/batchnorm_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/batchnorm_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/blas.c" "CMakeFiles/darknetp.dir/host/src/blas.o" "gcc" "CMakeFiles/darknetp.dir/host/src/blas.o.d"
+  "/workspace/benchmark/darknetz/host/src/box.c" "CMakeFiles/darknetp.dir/host/src/box.o" "gcc" "CMakeFiles/darknetp.dir/host/src/box.o.d"
+  "/workspace/benchmark/darknetz/host/src/col2im.c" "CMakeFiles/darknetp.dir/host/src/col2im.o" "gcc" "CMakeFiles/darknetp.dir/host/src/col2im.o.d"
+  "/workspace/benchmark/darknetz/host/src/connected_layer.c" "CMakeFiles/darknetp.dir/host/src/connected_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/connected_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/convolutional_layer.c" "CMakeFiles/darknetp.dir/host/src/convolutional_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/convolutional_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/cost_layer.c" "CMakeFiles/darknetp.dir/host/src/cost_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/cost_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/crnn_layer.c" "CMakeFiles/darknetp.dir/host/src/crnn_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/crnn_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/crop_layer.c" "CMakeFiles/darknetp.dir/host/src/crop_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/crop_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/cuda.c" "CMakeFiles/darknetp.dir/host/src/cuda.o" "gcc" "CMakeFiles/darknetp.dir/host/src/cuda.o.d"
+  "/workspace/benchmark/darknetz/host/src/data.c" "CMakeFiles/darknetp.dir/host/src/data.o" "gcc" "CMakeFiles/darknetp.dir/host/src/data.o.d"
+  "/workspace/benchmark/darknetz/host/src/deconvolutional_layer.c" "CMakeFiles/darknetp.dir/host/src/deconvolutional_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/deconvolutional_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/demo.c" "CMakeFiles/darknetp.dir/host/src/demo.o" "gcc" "CMakeFiles/darknetp.dir/host/src/demo.o.d"
+  "/workspace/benchmark/darknetz/host/src/detection_layer.c" "CMakeFiles/darknetp.dir/host/src/detection_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/detection_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/diffprivate.c" "CMakeFiles/darknetp.dir/host/src/diffprivate.o" "gcc" "CMakeFiles/darknetp.dir/host/src/diffprivate.o.d"
+  "/workspace/benchmark/darknetz/host/src/dropout_layer.c" "CMakeFiles/darknetp.dir/host/src/dropout_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/dropout_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/gemm.c" "CMakeFiles/darknetp.dir/host/src/gemm.o" "gcc" "CMakeFiles/darknetp.dir/host/src/gemm.o.d"
+  "/workspace/benchmark/darknetz/host/src/gru_layer.c" "CMakeFiles/darknetp.dir/host/src/gru_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/gru_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/im2col.c" "CMakeFiles/darknetp.dir/host/src/im2col.o" "gcc" "CMakeFiles/darknetp.dir/host/src/im2col.o.d"
+  "/workspace/benchmark/darknetz/host/src/image.c" "CMakeFiles/darknetp.dir/host/src/image.o" "gcc" "CMakeFiles/darknetp.dir/host/src/image.o.d"
+  "/workspace/benchmark/darknetz/host/src/iseg_layer.c" "CMakeFiles/darknetp.dir/host/src/iseg_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/iseg_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/l2norm_layer.c" "CMakeFiles/darknetp.dir/host/src/l2norm_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/l2norm_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/layer.c" "CMakeFiles/darknetp.dir/host/src/layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/list.c" "CMakeFiles/darknetp.dir/host/src/list.o" "gcc" "CMakeFiles/darknetp.dir/host/src/list.o.d"
+  "/workspace/benchmark/darknetz/host/src/local_layer.c" "CMakeFiles/darknetp.dir/host/src/local_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/local_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/logistic_layer.c" "CMakeFiles/darknetp.dir/host/src/logistic_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/logistic_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/lstm_layer.c" "CMakeFiles/darknetp.dir/host/src/lstm_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/lstm_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/matrix.c" "CMakeFiles/darknetp.dir/host/src/matrix.o" "gcc" "CMakeFiles/darknetp.dir/host/src/matrix.o.d"
+  "/workspace/benchmark/darknetz/host/src/maxpool_layer.c" "CMakeFiles/darknetp.dir/host/src/maxpool_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/maxpool_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/network.c" "CMakeFiles/darknetp.dir/host/src/network.o" "gcc" "CMakeFiles/darknetp.dir/host/src/network.o.d"
+  "/workspace/benchmark/darknetz/host/src/normalization_layer.c" "CMakeFiles/darknetp.dir/host/src/normalization_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/normalization_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/option_list.c" "CMakeFiles/darknetp.dir/host/src/option_list.o" "gcc" "CMakeFiles/darknetp.dir/host/src/option_list.o.d"
+  "/workspace/benchmark/darknetz/host/src/parser.c" "CMakeFiles/darknetp.dir/host/src/parser.o" "gcc" "CMakeFiles/darknetp.dir/host/src/parser.o.d"
+  "/workspace/benchmark/darknetz/host/src/region_layer.c" "CMakeFiles/darknetp.dir/host/src/region_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/region_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/reorg_layer.c" "CMakeFiles/darknetp.dir/host/src/reorg_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/reorg_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/rnn_layer.c" "CMakeFiles/darknetp.dir/host/src/rnn_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/rnn_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/route_layer.c" "CMakeFiles/darknetp.dir/host/src/route_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/route_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/shortcut_layer.c" "CMakeFiles/darknetp.dir/host/src/shortcut_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/shortcut_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/softmax_layer.c" "CMakeFiles/darknetp.dir/host/src/softmax_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/softmax_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/tcp_transfer.c" "CMakeFiles/darknetp.dir/host/src/tcp_transfer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/tcp_transfer.o.d"
+  "/workspace/benchmark/darknetz/host/src/tree.c" "CMakeFiles/darknetp.dir/host/src/tree.o" "gcc" "CMakeFiles/darknetp.dir/host/src/tree.o.d"
+  "/workspace/benchmark/darknetz/host/src/upsample_layer.c" "CMakeFiles/darknetp.dir/host/src/upsample_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/upsample_layer.o.d"
+  "/workspace/benchmark/darknetz/host/src/utils.c" "CMakeFiles/darknetp.dir/host/src/utils.o" "gcc" "CMakeFiles/darknetp.dir/host/src/utils.o.d"
+  "/workspace/benchmark/darknetz/host/src/yolo_layer.c" "CMakeFiles/darknetp.dir/host/src/yolo_layer.o" "gcc" "CMakeFiles/darknetp.dir/host/src/yolo_layer.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ benchmark/hotp/build/CMakeFiles/optee_example_hotp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/optee_example_hotp.dir/host/main.o"
+  "CMakeFiles/optee_example_hotp.dir/host/main.o.d"
+  "optee_example_hotp"
+  "optee_example_hotp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/optee_example_hotp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
