8f8945fefa62f46f79a545946461d2e747123c20
ta-061 <cstt22111@g.nihon-u.ac.jp>
2025-10-14 02:17:17 +0900

    gpt-5-nano使用時の全体ベンチ実行結果

--- benchmark/aes/ta/.user_ta_header.o.d
+++ benchmark/aes/ta/.user_ta_header.o.d
@@ -7,13 +7,13 @@ user_ta_header.o: \
  /workspace/optee_os/out/arm/export-ta_arm32/include/inttypes.h \
  /workspace/optee_os/out/arm/export-ta_arm32/include/stdint.h \
  /workspace/optee_os/out/arm/export-ta_arm32/include/limits.h \
- /mnt/disk/toolschain/gcc-linaro-6.5.0-2018.12-x86_64_arm-linux-gnueabihf/lib/gcc/arm-linux-gnueabihf/6.5.0/include/stdbool.h \
- /mnt/disk/toolschain/gcc-linaro-6.5.0-2018.12-x86_64_arm-linux-gnueabihf/lib/gcc/arm-linux-gnueabihf/6.5.0/include/stddef.h \
+ /usr/lib/gcc-cross/arm-linux-gnueabihf/11/include/stdbool.h \
+ /usr/lib/gcc-cross/arm-linux-gnueabihf/11/include/stddef.h \
  /workspace/optee_os/out/arm/export-ta_arm32/include/tee_internal_api_extensions.h \
  /workspace/optee_os/out/arm/export-ta_arm32/include/trace.h \
  /workspace/optee_os/out/arm/export-ta_arm32/include/trace_levels.h \
  /workspace/optee_os/out/arm/export-ta_arm32/include/stdio.h \
- /mnt/disk/toolschain/gcc-linaro-6.5.0-2018.12-x86_64_arm-linux-gnueabihf/lib/gcc/arm-linux-gnueabihf/6.5.0/include/stdarg.h \
+ /usr/lib/gcc-cross/arm-linux-gnueabihf/11/include/stdarg.h \
  /workspace/optee_os/out/arm/export-ta_arm32/include/tee_api_defines_extensions.h \
  /workspace/optee_os/out/arm/export-ta_arm32/include/user_ta_header.h \
  /workspace/optee_os/out/arm/export-ta_arm32/include/util.h \
--- benchmark/basicAlg_use/ta/.CryptoVerifyTaPbkdf2.o.d
+++ benchmark/basicAlg_use/ta/.CryptoVerifyTaPbkdf2.o.d
@@ -2,14 +2,14 @@ CryptoVerifyTaPbkdf2.o: CryptoVerifyTaPbkdf2.c \
  include/CryptoVerifyTaPbkdf2.h include/CryptoVerifyTaType.h \
  /workspace/optee_os/out/arm/export-ta_arm32/include/tee_internal_api.h \
  /workspace/optee_os/out/arm/export-ta_arm32/include/tee_api.h \
- /mnt/disk/toolschain/gcc-linaro-6.5.0-2018.12-x86_64_arm-linux-gnueabihf/lib/gcc/arm-linux-gnueabihf/6.5.0/include/stddef.h \
+ /usr/lib/gcc-cross/arm-linux-gnueabihf/11/include/stddef.h \
  /workspace/optee_os/out/arm/export-ta_arm32/include/compiler.h \
  /workspace/optee_os/out/arm/export-ta_arm32/include/tee_api_defines.h \
  /workspace/optee_os/out/arm/export-ta_arm32/include/tee_api_types.h \
  /workspace/optee_os/out/arm/export-ta_arm32/include/inttypes.h \
  /workspace/optee_os/out/arm/export-ta_arm32/include/stdint.h \
  /workspace/optee_os/out/arm/export-ta_arm32/include/limits.h \
- /mnt/disk/toolschain/gcc-linaro-6.5.0-2018.12-x86_64_arm-linux-gnueabihf/lib/gcc/arm-linux-gnueabihf/6.5.0/include/stdbool.h \
+ /usr/lib/gcc-cross/arm-linux-gnueabihf/11/include/stdbool.h \
  /workspace/optee_os/out/arm/export-ta_arm32/include/trace.h \
  /workspace/optee_os/out/arm/export-ta_arm32/include/trace_levels.h \
  /workspace/optee_os/out/arm/export-ta_arm32/include/tee_ta_api.h \
